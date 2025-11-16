c42d64f0b1e37d54a2750cf32b81601944bcbaa8
Yonas <116105802+yonas255@users.noreply.github.com>
2025-11-16 03:08:55 +0000

    Updated tests, fixtures, and post model validations; test suite now fully passing

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is valid with title and body" do
+    post = Post.new(title: "test title", body: "test Body Content")
+    assert post.valid?
+  end
+
+  test "is invalid without a Title" do
+    post = Post.new(body: "Content only")
+    assert_not post.valid?
+    assert_includes post.errors[:title], "can't be blank"
+  end
+
+  test "is invalid without a body" do
+    post = Post.new(title: "title only")
+    assert_not post.valid?
+    assert_includes post.errors[:body], "can't be blank"
+  end
 end
