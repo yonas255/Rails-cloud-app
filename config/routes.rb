df36386fd167968aa53d9772efe7268698ac36aa
Yonas <116105802+yonas255@users.noreply.github.com>
2025-11-18 03:53:38 +0000

    Update application layout with navbar and flash messages

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,5 @@ Rails.application.routes.draw do
 
   root "pages#home"
 
-  unauthenticated do
-    root "pages#home", as: :unauthenticated_root
-  end
-
-  if Rails.env.production?
-    get "/run_migration", to: "pages#run_migrations"
-  end
+    get "/run_migration", to: "pages#run_migrations" if Rails.env.production?
 end
