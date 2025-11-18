b9f4dd86260841885ba84b902bc233ebf9da3a42
Yonas <116105802+yonas255@users.noreply.github.com>
2025-11-18 01:17:17 +0000

    Add migration runner

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def run_migrations
+    ActiveRecord::MigrationContext.new("db/migrate", ActiveRecord::SchemaMigration)
+    render plain: "Migration completed successfully!"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,8 @@ Rails.application.routes.draw do
   unauthenticated do
     root "pages#home", as: :unauthenticated_root
   end
+
+  if Rails.env.production?
+    get "/run_migration", to: "pages#run_migrations"
+  end
 end
